0bb65f87d28f1266c5aee6b70688f09f3388ae5a
Kevin Espinoza <kcesp1996@gmail.com>
2025-10-07 15:49:52 -0700

    updated city tile id to follow naming convention

--- modules/php/material.inc.php
+++ modules/php/material.inc.php
@@ -8,13 +8,13 @@ $this->MAP_INTRO_2P = array (
     '3,-5' => array ('type' => 'land','rotation' => 270,'id' => 'landtile_8' ),
     '-1,-3' => array ('type' => 'land','rotation' => 0,'id' => 'landtile_9' ),
     '-1,1' => array ('type' => 'land','rotation' => 180,'id' => 'landtile_3' ),
-    '0,0' => array ('type' => 'city','rotation' => 0,'id' => 0 ),
-    '-2,2' => array ('type' => 'city','rotation' => 0,'id' => 1 ),
-    '-4,0' => array ('type' => 'city','rotation' => 0,'id' => 2 ),
-    '4,2' => array ('type' => 'city','rotation' => 0,'id' => 3 ),
-    '2,-2' => array ('type' => 'city','rotation' => 0,'id' => 4 ),
-    '4,-2' => array ('type' => 'city','rotation' => 0,'id' => 5 ),
-    '0,-4' => array ('type' => 'city','rotation' => 0,'id' => 6 )
+    '0,0' => array ('type' => 'city','rotation' => 0,'id' => 'citytile_0' ),
+    '-2,2' => array ('type' => 'city','rotation' => 0,'id' => 'citytile_1' ),
+    '-4,0' => array ('type' => 'city','rotation' => 0,'id' => 'citytile_2' ),
+    '4,2' => array ('type' => 'city','rotation' => 0,'id' => 'citytile_3' ),
+    '2,-2' => array ('type' => 'city','rotation' => 0,'id' => 'citytile_4' ),
+    '4,-2' => array ('type' => 'city','rotation' => 0,'id' => 'citytile_5' ),
+    '0,-4' => array ('type' => 'city','rotation' => 0,'id' => 'citytile_6' )
 );
 
 $this->MAP_INTRO_3P = array (
@@ -28,12 +28,12 @@ $this->MAP_INTRO_3P = array (
     '1,-9' => array ('type' => 'land','rotation' => 0,'id' => 'landtile_3' ),
     '5,-9' => array ('type' => 'land','rotation' => 90,'id' => 'landtile_4' ),
     '3,-11' => array ('type' => 'land','rotation' => 180,'id' => 'landtile_5' ),
-    '0,-2' => array ('type' => 'city','rotation' => 0,'id' => 0 ),
-    '2,-6' => array ('type' => 'city','rotation' => 0,'id' => 1 ),
-    '0,-6' => array ('type' => 'city','rotation' => 0,'id' => 2 ),
-    '4,-6' => array ('type' => 'city','rotation' => 0,'id' => 3 ),
-    '6,-8' => array ('type' => 'city','rotation' => 0,'id' => 4 ),
-    '0,-10' => array ('type' => 'city','rotation' => 0,'id' => 5 ),
-    '2,-10' => array ('type' => 'city','rotation' => 0,'id' => 6 ),
-    '4,-10' => array ('type' => 'city','rotation' => 0,'id' => 7 )
+    '0,-2' => array ('type' => 'city','rotation' => 0,'id' => 'citytile_0' ),
+    '2,-6' => array ('type' => 'city','rotation' => 0,'id' => 'citytile_1' ),
+    '0,-6' => array ('type' => 'city','rotation' => 0,'id' => 'citytile_2' ),
+    '4,-6' => array ('type' => 'city','rotation' => 0,'id' => 'citytile_3' ),
+    '6,-8' => array ('type' => 'city','rotation' => 0,'id' => 'citytile_4' ),
+    '0,-10' => array ('type' => 'city','rotation' => 0,'id' => 'citytile_5' ),
+    '2,-10' => array ('type' => 'city','rotation' => 0,'id' => 'citytile_6' ),
+    '4,-10' => array ('type' => 'city','rotation' => 0,'id' => 'citytile_7' )
 );
